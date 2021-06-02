2511956a9a8b5e25eabdae470484431be89bfa4d
Kamakshaiah <dr.m.kamakshaiah@gmail.com>
2021-06-02 21:37:33 +0530

    added several functions (tests) in literstat module

--- literstat.py
+++ literstat.py
@@ -1,3 +1,4 @@
+# Univariate
 def computeIQR(v1):
     ''' Computes Inter Quartile Range (IQR) for word's frequency - input should be univariate distribution '''
 
@@ -5,12 +6,33 @@ def computeIQR(v1):
     stat = stats.iqr(v1, axis=0)
     return stat
 
+# normality test
+def ShapiroTest(v):
+    ''' Performs Shapiros Test of normality '''
+    from scipy.stats import shapiro
+    stat, p = shapiro(data)
+    return {'stat': stat, 'p': p}
+
+def DAgostinosK2Test(v):
+    ''' Performs D’Agostino’s K^2 Test of normality '''
+    from scipy.stats import normaltest
+    stat, p = normaltest(data)
+    return {'stat': stat, 'p': p}
+
+def AndersonDarlingTest(v):
+    ''' Performs Anderson-Darling Test of normality '''
+    from scipy.stats import normaltest
+    stat, p = normaltest(data)
+    return {'stat': stat, 'p': p}
+
 def oneSampleTTest(v1, mean=0):
     ''' Performs one sample T Test for given word frequency vector '''
     from scipy import stats
     res = stats.ttest_1samp(v1, mean)
     return res
 
+# dependencies
+    
 def twoSampleIndTTest(v1, v2):
     ''' Performs two sample ind. T Test for two different samples - for termmat and distinctwords distributions '''
     
@@ -18,7 +40,7 @@ def twoSampleIndTTest(v1, v2):
     res = stats.ttest_ind(v1, v2)
     return res
 
-def moodsTest(v1, v2, ties=True):
+def MoodsTest(v1, v2, ties=True):
     ''' Performs moods test for two different serach phrases '''
 
     from scipy.stats import median_test
@@ -38,7 +60,19 @@ def moodsTest(v1, v2, ties=True):
             
         return {'g': g, 'p': p, 'med': med, 'tbl': tbl}
 
-def bartlettTest(v1, v2):
+def chiSqContingencyTest(tbl):
+    ''' performs chi2 contingency test on table returned from mood's test '''
+    import scipy.stats as stats
+    out = stats.chi2_contingency(tbl)
+    return out
+
+def FishersExactTest(tbl):
+    ''' performs chi2 contingency test on table returned from mood's test '''
+    import scipy.stats as stats
+    out = stats.fisher_exact(tbl)
+    return out
+    
+def BartlettTest(v1, v2):
         ''' Useful for performing bartletts test of spherecity. Input data should be numerical (may be word frequencies) '''
         from scipy.stats import bartlett
 
@@ -46,21 +80,72 @@ def bartlettTest(v1, v2):
         
         return {'stat': stat, 'p-value': p}
 
+def ANOVA(v1, v2):
+    ''' Tests whether the means of two or more independent samples are significantly different. '''
+    from scipy.stats import f_oneway
+    stat, p = f_oneway(v1, v2)
+    return {'stat': stat, 'p-value': p}    
+
+# non-parametric tests
+
+def MannWhitneyUTest(v1, v2):
+    ''' Tests whether the distributions of two independent samples are equal or not. '''
+    from scipy.stats import mannwhitneyu
+    stat, p = mannwhitneyu(v1, v2)
+    return {'stat': stat, 'p-value': p}
+
+def WilcoxonSignedRankTest(v1, v2):
+    ''' Tests whether the distributions of two independent samples are equal or not.
+        Caution: Samples must have the same length '''
+    
+    from scipy.stats import wilcoxon
+    stat, p = wilcoxon(v1, v2)
+    return {'stat': stat, 'p-value': p}
+
+def KruskalWallisHTest(v1, v2):
+    ''' Tests whether the distributions of two independent samples are equal or not. '''
+    from scipy.stats import kruskal
+    stat, p = kruskal(v1, v2)
+    return {'stat': stat, 'p-value': p}
+
+def FriedmanTest(v1, v2, v3):
+    ''' Tests whether the distributions of two independent samples are equal or not. '''
+    from scipy.stats import friedmanchisquare
+    stat, p = friedmanchisquare(v1, v2, v3)
+    return {'stat': stat, 'p-value': p}
+
+# associations
+def PearsonsCorrelationSigTest(v1, v2):
+    ''' Provides Karl Pearson's r and p value for significant test
+        Caution: Samples must have the same length '''
+    from scipy.stats import pearsonr
+    stat, p = pearsonr(v1, v2)
+    return {'stat': stat, 'p': p}
+
+def SpearmansRankCorrelation(v1, v2):
+    ''' Provides Spearman’s Rho and p value for significant test
+        Caution: Samples must have the same length '''
+    
+    from scipy.stats import spearmanr
+    stat, p = spearmanr(v1, v2)
+    return {'stat': stat, 'p': p}
+
+def KendallsRankCorrelation(v1, v2):
+    ''' Provides Kendalls Tau and p value for significant test
+        Caution: Samples must have the same length '''
+
+    from scipy.stats import kendalltau
+    stat, p = kendalltau(v1, v2)
+    return {'stat': stat, 'p': p}
+
 def barChart(df):
 
     ''' Creates barchart for termmat (TM)/distinct words matrix (DWM)
         params: df [pandas dataframe] '''
 
-##    import pandas as pd
-##    df = df.head()
-##    df.plot.bar(df['words'], df['freq'])
-##    plot.show()
-##    
-
     import matplotlib.pyplot as plt
     columns = list(df.keys())
     plt.bar(df[columns[0]], df[columns[1]])
-##    plt.xticks(rotation=45)
     plt.show()
 
 def pieChart(df):
@@ -200,12 +285,18 @@ if __name__ == '__main__':
     import numpy as np
     v1 = np.random.randint(1, 10, 30)
     v2 = np.random.randint(1, 10, 30)
+    v3 = np.random.randint(1, 10, 30)
 ##    print(bartlettTest(v1, v2))
 ##    print(computeIQR(v1))
-##    print(moodsTest(v1, v2))
-    print(oneSampleTTest(v1))
-    import pandas as pd
-    data = {'v1': v1, 'v2':v2}
-    df = pd.DataFrame(data)
-    print(biVariateClusterAnalysis(df, plot=True))
-    
+    mout = MoodsTest(v1, v2)
+##    print(oneSampleTTest(v1))
+##    import pandas as pd
+##    data = {'v1': v1, 'v2':v2}
+##    df = pd.DataFrame(data)
+##    print(biVariateClusterAnalysis(df, plot=True))
+##    print(chiSqContingencyTest(mout['tbl']))
+##    print(ANOVA(v1, v2))
+##    print(FriedmanTest(v1, v2, v3))
+##    print(PearsonsCorrelationSigTest(v1, v2))
+##    print(SpearmansRankCorrelation(v1, v2))
+    print(WilcoxonSignedRankTest(v1, v2))
